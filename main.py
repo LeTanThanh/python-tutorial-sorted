7c817fdb86fe9285dfc7e7bbbe366665d34d0e8f
TanThanhLe <tanthanhcr95@gmail.com>
2024-04-30 17:49:31 +0700

    Using Python sorted() function to sort a list of strings

--- main.py
+++ main.py
@@ -5,3 +5,15 @@ if __name__ == "__main__":
   sorted(list)
   sorted(list, reverse = True)
   """
+
+  # Using Python sorted() function to sort a list of strings
+
+  guests = ["James", "Mary", "John", "Patricia", "Robert", "Jennifer"]
+  sorted_guests = sorted(guests)
+  print(guests)
+  print(sorted_guests)
+
+  guests = ["James", "Mary", "John", "Patricia", "Robert", "Jennifer"]
+  sorted_guests = sorted(guests, reverse = True)
+  print(guests)
+  print(sorted_guests)
